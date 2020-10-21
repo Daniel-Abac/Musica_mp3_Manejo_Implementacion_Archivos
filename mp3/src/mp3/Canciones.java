1a47ca540535b9292afb3b13a5e67068205856a3
Diego090200 <38775456+Diego090200@users.noreply.github.com>
2020-10-20 22:42:15 -0600

    Add files via upload
    Se creo la clase canciones

---
+++ mp3/src/mp3/Canciones.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mp3;
+
+/**
+ *
+ * @author alehe
+ */
+
+public class Canciones {
+    private String disquera;
+    private String artista;
+    private String album;
+    private String año;
+    private String nombreC;
+    private String genero;
+    private String direccionC;
+    private String duracion; 
+    private String urlAutor;
+    private String urlDisquera;
+    private String biografia;
+    
+    public Canciones(){
+        this.disquera = new String();
+        this.artista =  new String();
+        this.album =  new String();
+        this.año =  new String();
+        this.nombreC = new String();
+        this.genero = new String();
+        this.direccionC =  new String();
+        this.duracion = new String();;
+        this.urlAutor =  new String();
+        this.urlDisquera =  new String();
+        this.biografia =  new String();
+    }
+}
