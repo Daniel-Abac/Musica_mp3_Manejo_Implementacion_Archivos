248243afd029efffc837b3e265993e30f36e70d2
Daniel-Abac <43415220+Daniel-Abac@users.noreply.github.com>
2020-11-11 23:12:11 -0600

    Cambio de Frame
    Se cambio el frame principal

--- mp3/src/Ventanas/Principal.java
+++ mp3/src/Ventanas/Principal.java
@@ -5,22 +5,90 @@
  */
 package Ventanas;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.ImageIcon;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 /**
  *
  * @author Daniel
  */
 public class Principal extends javax.swing.JFrame {
 
-    /**
-     * Creates new form Principal
-     */
+    private ArrayList<MP3> data;
+    DefaultTableModel modelo;
+    private String cancionAEditar = "";
     public Principal() {
         initComponents();
         this.setLocationRelativeTo(null);
+    }
+    
+    
+    public void getData() throws FileNotFoundException, IOException {
+       jTable1.getTableHeader().setFont(new Font("Arial", 1, 13));
+        // cambia el fondo del encabezado de la tabla
+        jTable1.getTableHeader().setBackground(Color.WHITE);
+        // cambia el color de la letra del encabezado de la tabla
+        jTable1.getTableHeader().setForeground(Color.BLACK);
+        modelo = new DefaultTableModel();
+        modelo.addColumn("Cancion");
+        modelo.addColumn("Artista");
+        modelo.addColumn("Album");
+        RandomAccessFile lectura = new RandomAccessFile("canciones.data", "rw");
+        short punteroIndice = lectura.readShort();
+        lectura.seek(punteroIndice);
+        for (int i = 0; i < lectura.length(); i++) {//Recorre el archivo para obtener los indices
+            short posicionDatos = lectura.readShort();
+            byte longitud = lectura.readByte();
+            byte[] caracteres = new byte[longitud * 2];
+            lectura.read(caracteres);
+            String cancion = new String(caracteres);
+            modelo.addRow(new Object[]{cancion, this.getArtista(posicionDatos), this.getAlbum(posicionDatos)});
+            if (lectura.getFilePointer() == lectura.length()) {//Rompe si llega al final del archivo
+                break;
+            }
+        }
+        this.jTable1.setModel(modelo);
+    }
+
+    public String getArtista(int posicion) throws IOException {
+        RandomAccessFile lectura = new RandomAccessFile("canciones.data", "rw");
+        lectura.seek(posicion);
+        byte longitud = lectura.readByte();
+        byte[] caracteres = new byte[longitud * 2];
+        lectura.read(caracteres);
+        String cancion = new String(caracteres);
+        return cancion;
+    }
+
+    public String getAlbum(int posicion) throws IOException {
+        RandomAccessFile lectura = new RandomAccessFile("canciones.data", "rw");
+        lectura.seek(posicion);
+        byte longitud = lectura.readByte();
+        byte[] caracteres = new byte[longitud * 2];
+        lectura.read(caracteres);
+        String cancion = new String(caracteres);
+        byte longitud1 = lectura.readByte();
+        byte[] caracteres1 = new byte[longitud1 * 2];
+        lectura.read(caracteres1);
+        String cancion1 = new String(caracteres1);
+        return cancion1;
+    }
+
+    public void AgregarCarpetaMusica() throws IOException {
+        JFileChooser jf = new JFileChooser();
+        jf.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        jf.showSaveDialog(null);
+        LectorArchivos lee = new LectorArchivos(jf.getSelectedFile().getAbsolutePath());
+        lee.SaveInformation();
     }
 
     /**
@@ -32,69 +100,93 @@ public class Principal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jDesktopPaneEscritorio = new javax.swing.JDesktopPane();
-        jMenuBar1 = new javax.swing.JMenuBar();
-        jMenuAgregar = new javax.swing.JMenu();
-        jCheckBoxMenuItemAgregarRuta = new javax.swing.JCheckBoxMenuItem();
+        jLabel1 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        jLabel2 = new javax.swing.JLabel();
+        jButtonVer_Editar = new javax.swing.JButton();
+        jToggleButton1 = new javax.swing.JToggleButton();
+        Fondo = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Principal");
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jDesktopPaneEscritorio.setToolTipText("");
-        jDesktopPaneEscritorio.setName(""); // NOI18N
+        jLabel1.setFont(new java.awt.Font("Times New Roman", 0, 24)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setText("Lista De Canciones");
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 30, -1, -1));
 
-        javax.swing.GroupLayout jDesktopPaneEscritorioLayout = new javax.swing.GroupLayout(jDesktopPaneEscritorio);
-        jDesktopPaneEscritorio.setLayout(jDesktopPaneEscritorioLayout);
-        jDesktopPaneEscritorioLayout.setHorizontalGroup(
-            jDesktopPaneEscritorioLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 800, Short.MAX_VALUE)
-        );
-        jDesktopPaneEscritorioLayout.setVerticalGroup(
-            jDesktopPaneEscritorioLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 738, Short.MAX_VALUE)
-        );
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(jTable1);
 
-        jMenuBar1.setToolTipText("Principal");
+        getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 80, 660, 230));
 
-        jMenuAgregar.setText("Agregar");
+        jLabel2.setBackground(new java.awt.Color(255, 255, 255));
+        jLabel2.setFont(new java.awt.Font("Times New Roman", 1, 12)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel2.setText("Ver o Editar");
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(720, 90, -1, -1));
 
-        jCheckBoxMenuItemAgregarRuta.setSelected(true);
-        jCheckBoxMenuItemAgregarRuta.setText("Agregar Ruta");
-        jCheckBoxMenuItemAgregarRuta.addActionListener(new java.awt.event.ActionListener() {
+        jButtonVer_Editar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagen/floppy-disk .png"))); // NOI18N
+        jButtonVer_Editar.setBorderPainted(false);
+        jButtonVer_Editar.setContentAreaFilled(false);
+        jButtonVer_Editar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jCheckBoxMenuItemAgregarRutaActionPerformed(evt);
+                jButtonVer_EditarActionPerformed(evt);
             }
         });
-        jMenuAgregar.add(jCheckBoxMenuItemAgregarRuta);
+        getContentPane().add(jButtonVer_Editar, new org.netbeans.lib.awtextra.AbsoluteConstraints(730, 110, 40, -1));
 
-        jMenuBar1.add(jMenuAgregar);
+        jToggleButton1.setText("jToggleButton1");
+        jToggleButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jToggleButton1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jToggleButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(720, 180, 80, -1));
 
-        setJMenuBar(jMenuBar1);
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jDesktopPaneEscritorio)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jDesktopPaneEscritorio)
-        );
+        Fondo.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        Fondo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagen/17041.jpg"))); // NOI18N
+        getContentPane().add(Fondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 820, 370));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jCheckBoxMenuItemAgregarRutaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBoxMenuItemAgregarRutaActionPerformed
-        // TODO add your handling code here:
+    private void jToggleButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jToggleButton1ActionPerformed
         try {
-            Ruta open = new Ruta();
-            jDesktopPaneEscritorio.add(open);
-            open.show();
+            this.AgregarCarpetaMusica();
+            this.getData();
         } catch (IOException ex) {
             Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
         }
-    }//GEN-LAST:event_jCheckBoxMenuItemAgregarRutaActionPerformed
+    }//GEN-LAST:event_jToggleButton1ActionPerformed
+
+    private void jButtonVer_EditarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonVer_EditarActionPerformed
+        if(cancionAEditar.equals("") == false){
+            EditarDatos datos = new EditarDatos(cancionAEditar);
+            try {
+                datos.MostrarDatosEnFrame();
+            } catch (IOException ex) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            datos.setVisible(true);
+        }else{
+            JOptionPane.showMessageDialog(null, "Seleccione en la tabla la canción a editar",
+                "IPORTANTE", JOptionPane.INFORMATION_MESSAGE);
+        }
+    }//GEN-LAST:event_jButtonVer_EditarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -132,10 +224,13 @@ public class Principal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JCheckBoxMenuItem jCheckBoxMenuItemAgregarRuta;
-    private javax.swing.JDesktopPane jDesktopPaneEscritorio;
-    private javax.swing.JMenu jMenuAgregar;
-    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JLabel Fondo;
+    private javax.swing.JButton jButtonVer_Editar;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
+    private javax.swing.JToggleButton jToggleButton1;
     // End of variables declaration//GEN-END:variables
 }
 
